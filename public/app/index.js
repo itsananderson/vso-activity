a90f698d353f656c0113994f618d9b980e71f390
Will Anderson <will@itsananderson.com>
2014-09-23 01:06:56 -0700

    Add basic (slow) frontend filtering

--- public/app/index.js
+++ public/app/index.js
@@ -2,13 +2,33 @@ angular.module('vso-activity', [])
     .service('activitySvc', function($http) {
         this.getActivity = function getActivity() {
             return $http.get('/api/get-activity/').then(function(result) {
-                console.log(result.data);
                 return result.data;
             });
         };
     })
-    .controller('activityCtrl', function(activitySvc) {
+    .service('authorFilterSvc', function() {
+        this.filter = '';
+    })
+    .filter('authorFilter', function(authorFilterSvc) {
+        return function(commits) {
+            var lowerSearch = authorFilterSvc.filter.toLowerCase();
+            console.log(commits);
+            return Object.keys(commits).map(function(key) {
+                return commits[key];
+            }).filter(function(commit) {
+                return -1 != commit.Author.Email.toLowerCase().indexOf(lowerSearch)
+                    || -1 != commit.Author.Name.toLowerCase().indexOf(lowerSearch)
+                    || -1 != commit.MessageShort.toLowerCase().indexOf(lowerSearch);
+            });
+        };
+    })
+    .controller('activityCtrl', function(authorFilterSvc, activitySvc) {
         var vm = this;
         vm.allActivity = [];
-        activitySvc.getActivity().then(function(activity) { console.log(activity); vm.allActivity = activity });
+        vm.filterSvc = authorFilterSvc;
+        vm.authorMatch= function filterMatch(value, index) {
+            console.log(arguments);
+            return -1 !== item.Sha.indexOf(vm.filter);
+        };
+        activitySvc.getActivity().then(function(activity) { vm.allActivity = activity });
     });
